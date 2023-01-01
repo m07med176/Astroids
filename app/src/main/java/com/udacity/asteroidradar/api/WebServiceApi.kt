ce7f155fa27112e788b974f7ed0c2d63fea09201
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-01-01 22:43:36 +0200

    Retrofit Web Service Api

---
+++ app/src/main/java/com/udacity/asteroidradar/api/WebServiceApi.kt
@@ -0,0 +1,25 @@
+package com.udacity.asteroidradar.api
+
+import com.udacity.asteroidradar.models.PictureOfDay
+import com.udacity.asteroidradar.utils.Constants
+import kotlinx.coroutines.Deferred
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WebServiceApi {
+
+    @GET("planetary/apod")
+    fun getDayPicture(
+        @Query("api_key") apiKey: String = Constants.API_KEY
+    ): Deferred<PictureOfDay>
+
+
+    @GET("neo/rest/v1/feed")
+    fun getAllAsteroidsData(
+        @Query("start_date") startDate:String,
+        @Query("api_key") apiKey : String = Constants.API_KEY)
+            : Deferred<ResponseBody>
+
+
+}
