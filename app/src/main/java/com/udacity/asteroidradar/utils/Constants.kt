a79ec87786874b8b284adab5ec61449f234e26a4
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-01-01 22:20:33 +0200

    ADD API Key To Constant

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -1,7 +1,11 @@
 package com.udacity.asteroidradar.utils
 
 object Constants {
+
+    // Retrofit
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    const val API_KEY = "4abbbcb6uCqhuv0AoYsVa4Up43BDsWghF8e1Vbpq"
+
 }
