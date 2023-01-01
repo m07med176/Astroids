a3c290495e69dde8d534049ba5f5087fd204dfb1
Mohamed Deiab <63822100+m07med176@users.noreply.github.com>
2023-01-01 22:35:01 +0200

    Retrofit Singlton

--- app/src/main/java/com/udacity/asteroidradar/api/RetrofitSingltone.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/RetrofitSingltone.kt
@@ -7,7 +7,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
-
+import com.udacity.asteroidradar.utils.Constants
 object RetrofitSingltone {
     private val client = OkHttpClient()
         .newBuilder()
